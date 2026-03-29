f59f99a85118edccf0b4ba33641ae8df81b621ea
Tobias Prechtl <tobiasprechtl45@gmail.com>
2026-03-30 00:17:05 +0200

    feat: initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Button>Hello</Button>
+    </div>
+  );
 }
