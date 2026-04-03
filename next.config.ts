b09477d3b8e0b6cdf31e018fcca550e98635153a
Tobias Prechtl <tobiasprechtl45@gmail.com>
2026-04-03 12:59:20 +0200

    feat: neon db support

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  allowedDevOrigins: ["192.168.*.*"],
+  allowedDevOrigins: ["192.168.*.*"], // only for testing on mobile
 };
 
 export default nextConfig;
--- src/lib/db-connection.ts
+++ src/lib/db-connection.ts
@@ -1,11 +1,20 @@
-import { drizzle } from "drizzle-orm/node-postgres";
+import { drizzle as drizzleNeon } from "drizzle-orm/neon-http";
+import { drizzle as drizzlePg } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 
-const db = drizzle(
-  new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: process.env.NODE_ENV === "production" ? true : false,
-  }),
-);
+function createDb() {
+  if (process.env.USE_NEON === "true") {
+    return drizzleNeon(process.env.DATABASE_URL!);
+  }
+
+  return drizzlePg(
+    new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: process.env.NODE_ENV === "production" ? true : false,
+    }),
+  );
+}
+
+const db = createDb();
 
 export { db };
